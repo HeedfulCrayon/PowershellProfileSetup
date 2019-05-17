ca3ceb6f3accc51121c0b62b52c5fed778b092f6
Nathan Borup <Nborup@DHICorp.com>
2019-05-17 13:27:43 -0600

    Initial Setup script and cmder settings added

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,11 @@
-function prompt {
-    write-host "$($env:username)@" -foregroundcolor "green" -nonewline
-    write-host "$($env:Userdomain)\$($env:computername)" -foregroundcolor "red" -nonewline
-    write-host " : " -foregroundcolor "gray" -nonewline
-    write-host $pwd -foregroundcolor "blue"
-    '> '
-}
 
 Remove-PSReadlineKeyHandler 'Ctrl+r'
+Import-Module Get-ChildItemColor
+Import-Module 'posh-git'
+Import-Module 'oh-my-posh'
+Set-Theme myHonukai
 Import-Module ZLocation
 Import-Module PSFzf
-Import-Module Get-ChildItemColor
 Set-alias -Name f -Value Invoke-fzf
 Set-Alias l Get-ChildItemColor -Option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
---
+++ setup.ps1
@@ -0,0 +1,29 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+cinst git.install -y
+cinst cmder -y
+Set-Location $env:USERPROFILE\Downloads\
+git clone https://github.com/powerline/fonts.git
+
+.\fonts\install.ps1
+
+#Import Cmder Settings
+#TODO figure out where chocolatey installs cmder
+
+#Install Modules and import them
+Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name 'PSFzf'
+Install-Module -Name 'ZLocation'
+Install-Module -Name 'posh-git'
+Install-Module -Name 'oh-my-posh'
+Install-Module -Name 'Get-ChildItemColor'
+Import-Module 'ZLocation'
+Import-Module 'PSFzf'
+Import-Module 'posh-git'
+Import-Module 'oh-my-posh'
+Import-Module 'Get-ChildItemColor'
+
+$poshVersion = (Get-ChildItem "C:\Program Files\WindowsPowerShell\Modules\oh-my-posh\" | Sort-Object -Property LastWriteTime | Select-Object -First 1).Name
+copy-item .\myHonukai.psm1 -Destination "C:\Program Files\WindowsPowerShell\Modules\oh-my-posh\$poshVersion\Themes\"
+Set-Theme myHonukai
