69e479b0ad3d5120fd8add0105ced8366fa20e28
Nathan Borup <Nborup@DHICorp.com>
2019-11-18 16:47:55 -0700

    Created download organization handlers

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -30,6 +30,52 @@ function Open-Solution {
   Set-Location $currentLocation
 }
 
+# Download organization
+
+function New-Folder {
+  param (
+    [string]$folderPath,
+    [string]$name
+  )
+  if (Test-Path -PathType Container $folderPath) {
+    New-Item -ItemType "directory" -Force -Path $folderPath -Name $name
+  }
+}
+
+# Create Folders
+New-Folder "$env:USERPROFILE\Downloads\" "Installs"
+New-Folder "$env:USERPROFILE\Downloads\" "Documents"
+New-Folder "$env:USERPROFILE\Downloads\" "Pictures"
+
+# Create Watcher
+$FileSystemWatcher = New-Object System.IO.FileSystemWatcher
+$FileSystemWatcher.Path  = "$env:USERPROFILE\Downloads"
+
+  Register-ObjectEvent -InputObject $FileSystemWatcher  -EventName Created  -Action {
+
+  $ext = [System.IO.Path]::GetExtension($Event.SourceEventArgs.Name)
+  $pathToCopyTo = ""
+  switch ($ext) {
+    {$_ -in ".pdf",".doc",".docx",".xls",".xlsx",".txt"} { $pathToCopyTo = "$env:USERPROFILE\Downloads\Documents\" }
+    {$_ -in ".jpg",".jpeg",".png",".gif"} { $pathToCopyTo = "$env:USERPROFILE\Downloads\Pictures\" }
+    {$_ -in ".exe",".msi"} { $pathToCopyTo = "$env:USERPROFILE\Downloads\Installs\" }
+  }
+  $Object  = "{0} was  {1} at {2} was copied to {3}" -f $Event.SourceEventArgs.FullPath, $Event.SourceEventArgs.ChangeType, $Event.TimeGenerated, $pathToCopyTo
+
+  $WriteHostParams  = @{
+
+  ForegroundColor = 'Green'
+
+  BackgroundColor =  'Black'
+
+  Object =  $Object
+
+  }
+  Move-Item $Event.SourceEventArgs.FullPath -Destination $pathToCopyTo -Force
+  Write-Host @WriteHostParams
+
+}
+
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
